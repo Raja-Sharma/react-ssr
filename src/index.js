732471835b0ffa8ad90803b9b07bbeab5c64ff62
Raja Sharma <rajasharma55@gmail.com>
2018-04-19 15:05:35 -0400

    Create redux stores. Beginning redux setup.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
+
 
 const app = express();
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
+  const store = createStore();
 
-  res.send(renderer(req));
+  // Some logic to initialize and 
+  // load data in to the store
+
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
